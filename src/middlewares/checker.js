81fdbfa7137d1223d0419ea632caf7ed81e0b9d3
Hunain Anis <hunainmuhammad@rocketmail.com>
2020-05-25 16:01:24 +0500

    Apply Middleware created and placed in folder with 3 files

---
+++ src/middlewares/checker.js
@@ -0,0 +1,22 @@
+import ADD_GOAL from '../actions/goals'
+import ADD_TODO from '../actions/todos'
+
+
+const checker = (store) => (next) => (action) => {
+    if(
+        action.type === ADD_TODO && 
+        action.todo.name.toLowerCase().includes('bitcoin')
+    ) {
+        return alert("Nope that's a bad Idea.")
+    }
+
+    if(
+        action.type === ADD_GOAL && 
+        action.goal.name.toLowerCase().includes('bitcoin')
+    ) {
+        return alert("Nope that's a bad Idea.")
+    }
+    return next(action)
+}
+
+export default checker
